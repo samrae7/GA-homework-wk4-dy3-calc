ef534a8803153ada8eca94c4833e6009d3d5e706
Sam Rae <samuelrae@gmail.com>
2015-09-03 09:21:01 +0100

    tried to do subtraction as well but failed. cant figure out how to pass the operation to the model ie call the subtract class and add class separately

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'sinatra/reloader' if development?
 require 'pry'
 
 require_relative './models/addition'
+require_relative './models/subtraction'
 
 
 get '/' do
@@ -13,12 +14,21 @@ get '/add' do
   erb :add
 end
 
+get '/subtract' do
+  erc :subtract
+end
+
 post '/addition' do
   redirect to "/#{params[:number1]}/#{params[:number2]}"
   binding.pry
 end
 
+post '/subtract' do
+  redirect to "/#{params[:number1]}/#{params[:number2]}"
+  binding.pry
+end
+
+
 get  '/:num1/:num2' do
   "#{Addition.add( params[:num1].to_i, params[:num2].to_i)}"
-  erb :home
 end
